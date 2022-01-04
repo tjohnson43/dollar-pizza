c9b6ca5aea10ccb127f34053bfc541d22f941936
Tanessia <tjohnson4391@richmondhillhs.org>
2022-01-04 16:53:00 +0000

    updated dollar pizza

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Plain from './plain';
-import Pepperoni from './pepperoni';
-import MeatLover from './meat-lover';
-import Veggie from './veggie';
+import Slice from './slice';
+import plain from './images/plain.jpg';
+import pepperoni from './images/pepperoni.jpg';
+import veggie from './images/veggie.jpeg';
+import meat from './images/meat.jpg';
+import hawaiian from './images/hawaiian.jpg';
+import bbq from './images/bbq.jpg';
 import './css/menu.css';
-import Hawaiian from './hawaiian';
-import Bbq from './bbq';
+
 
 function Menu() {
   return(
@@ -13,12 +15,41 @@ function Menu() {
       <h1 className='primary-text'>New York's World Famous</h1>
       <h1 className='secondary-text'>Pizza Menu</h1>
       <div className='row'>
-        <Plain/>
-        <Pepperoni/>
-        <MeatLover/>
-        <Veggie/>
-        <Hawaiian/>
-        <Bbq/>
+        <Slice  
+        img={plain}
+        title="plain slice"
+        description="It's got cheese, it's got tomato sauce, and it's only  a dollar. What more could you ask for?"
+        price="$1" />
+
+<Slice  
+        img={pepperoni}
+        title="pepperoni slice"
+        description="It's got cheese, it's got tomato sauce,have pepperoni, and it's only  a dollar. What more could you ask for?"
+        price="$1" />
+
+<Slice  
+        img={veggie}
+        title="veggie slice"
+        description="It's got cheese, it's got tomato sauce, with veggies, and it's only  a dollar. What more could you ask for?"
+        price="$1" />
+
+<Slice  
+        img={meat}
+        title="meat-lover slice"
+        description="It's got cheese, it's got tomato sauce, with meat, and it's only  a dollar. What more could you ask for?"
+        price="$1" />     
+
+          <Slice  
+        img={hawaiian}
+        title="hawaiian slice"
+        description="We know, you think this is gross. But try this once and we swear you'll be hooked on this pineapple and ham pizza."
+        price="$1" />   
+
+<Slice  
+        img={bbq}
+        title="bbq slice"
+        description="We know, you think this is gross. But try this once and we swear you'll be hooked on this Bbq pizza."
+        price="$1" />   
       </div>
     </div>
   );
